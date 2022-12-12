10c2ae252e2c97694134533050bc2f0886592a4c
KalHal <gemechukalkidan1@gmail.com>
2022-12-12 13:46:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,3 +9,9 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+import fileinput
+
+for lines in fileinput.FileInput("test.txt", inplace=1):
+	    lines = lines.strip()
+	        if lines == '': continue
+	        print lines
