4c81e370c4a75fc53f81fe76c69136071c2d57de
KalHal <gemechukalkidan1@gmail.com>
2022-12-12 13:50:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,14 @@
 *Return: o
 *
 */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 import fileinput
 
 for lines in fileinput.FileInput("test.txt", inplace=1):
 	    lines = lines.strip()
 	        if lines == '': continue
 	        print lines
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
