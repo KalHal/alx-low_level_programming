a1dd6eaa98b013aeb2edcb8687605b830c139b22
KaHal <gemechukalkidan1@gmail.com>
2022-12-17 11:51:06 -0800

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,9 +15,9 @@ int main(void)
 	/*your code goes on*/
 	if (n > 0)
 		printf("%i is postive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
+	else if (n < 0)
 		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
 	return (0);
 }
